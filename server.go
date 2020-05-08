21033bd90eed72967f31e2bd671341c44d5a3e04
After Ether <afterether@gmail.com>
2020-05-07 20:59:53 -0500

    market info, trades, volumes, and prices

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 import (
-	//"fmt"
+	"fmt"
+	"strconv"
 	"net/http"
 	"github.com/gin-gonic/gin"
+)
+const (
+	DEFAULT_MARKET_ROWS_LIMIT int	= 10
 )
 type AugurServer struct {
 	storage		*SQLStorage
@@ -22,11 +26,24 @@ func main_page(c *gin.Context) {
 //	c.String(http.StatusOK, "Hello %s %s", name,out)
 }
 func markets(c *gin.Context) {
-	blknum,_ := augur_srv.storage.get_last_block_num()
-	markets := augur_srv.storage.get_active_market_list()
+	off_str := c.Query("off")
+	var off int = 0
+	var err error
+	fmt.Printf("off_str = %v\n",off_str)
+	if len(off_str) > 0 {
+		off, err = strconv.Atoi(off_str)
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "error.html", gin.H{
+				"title": "Augur Markets: Error",
+				"ErrDescr": "Can't parse offset",
+			})
+			return
+		}
+	}
+	fmt.Printf("off = %v\n",off)
+	markets := augur_srv.storage.get_active_market_list(off,DEFAULT_MARKET_ROWS_LIMIT)
 	c.HTML(http.StatusOK, "markets.html", gin.H{
 			"title": "Augur Markets",
-			"block_num" : blknum,
 			"Markets" : markets,
 	})
 }
@@ -51,16 +68,19 @@ func explorer(c *gin.Context) {
 	blknum,res := augur_srv.storage.get_last_block_num()
 	_ = res
 	c.HTML(http.StatusOK, "explorer.html", gin.H{
-			"title": "Augur Market Categories",
+			"title": "Augur Event Explorer",
 			"block_num" : blknum,
 	})
 }
 func market_trades(c *gin.Context) {
 	market := c.Param("market")
+	market_info,_ := augur_srv.storage.get_market_info(market)
 	trades := augur_srv.storage.get_mkt_trades(market)
+	outcome_vols,_ := augur_srv.storage.get_outcome_volumes(market)
 	c.HTML(http.StatusOK, "market_info.html", gin.H{
 			"title": "Trades for market",
 			"Trades" : trades,
-			"market_address": market,
+			"Market": market_info,
+			"OutcomeVols" : outcome_vols,
 	})
 }
